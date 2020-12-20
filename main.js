299747264f16528dba71d1d2f13b2c788c41a4d2
Beacko <76181994+beacko28@users.noreply.github.com>
2020-12-20 14:19:45 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,53 @@
+//PARTE 1 - Introdução ao JS
+//var nome = "Rafael Galleani";
+//var idade = 29;
+//var idade2 = 10;
+//var frase = "Japão é o melhor time do mundo";
+//alert(nome + "tem" + idade + "anos" );
+//alert(idade + idade2);
+//console.log(nome);
+//console.log(idade + idade2);
+//console.log(frase.toUpperCase("Japão" , "Brasil"));
+//alert(frase.replace("Japão" , "Brasil"));
+
+//PARTE 2 - Array e Dicionário
+//var lista = ["maça, pêra, banana, manga, abacaxi"];
+//console.log(lista.join(" - ")); // Invés de virgula, transforma em string, e coloca o que vocÊ quiser (espaç e - no exemplo)
+//console.log(lista.toString()); // Transforma em String
+//console.log(lista.reverse()); // Traz a ordem dos elementos revertida
+//console.log(lista.length); // exibe a quantidade de elementos do array
+//lista.push("uva"); //push adiciona o elemento no array
+//lista.pop() //pop retira o elemento
+//console.log(lista[1]); // array sempre começa no 0
+//alert(lista[1]);
+//var fruta = (nome = "maça" , cor = "vermelha" ) // dicionário
+
+//PARTE 3 - Condicionais, Laços de Repetição e Date
+//var idade = prompt ("Qual a sua idade?");
+//var idade = 18;
+//if (idade >= 18) {
+//    alert("Maior de Idade");
+//} else {
+//    alert("Menor de Idade");
+//}
+
+/*var count = 0;
+while (count <= 5){
+    console.log(count);
+    alert(count);
+    count = count + 1; // count +1 ou count ++ é a mesma coisa
+}
+*/
+
+/*
+var count;
+for (count 0; count <* 5, count ++){
+    alert(count);
+}
+*/
+
+//var d = new Date(); //apresentar data + horario
+//alert(d); 
+//alert(d.getMonth()+1); // apresenta a data
+            // como o month começa do zero, o +1 intercede para o mês atual
+//alert(d.getMinutes()); // Apresenta os minutos
